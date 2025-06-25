a2ff5105d0d23e08ad48cd19f8dc90302f4c7abd
duc.ht <duc.hoang01@ipos.vn>
2025-06-25 14:57:42 +0700

    update database name

--- django_postgresql/settings.py
+++ django_postgresql/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = "django_postgresql.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django-postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
         "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
         "HOST": os.environ.get("DATABASE_HOST"),
